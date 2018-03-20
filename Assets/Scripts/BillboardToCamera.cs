555f78d7eea2b45b0768d26506ef85b930e3562b
EJ Go <emmanuelle.james.go@gmail.com>
2018-03-20 03:39:13 -0400

    Initial commit for PROJECT: Neon

---
+++ Assets/Scripts/BillboardToCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardToCamera : MonoBehaviour {
+
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update () {
+        Quaternion camRot = cam.transform.rotation; // makes code easier to read
+
+        transform.LookAt(transform.position + camRot * Vector3.forward, camRot * Vector3.up); // has the object billboard towards the camera
+	}
+}
