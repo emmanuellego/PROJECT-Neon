b53aef2d3d036d15b84a9f821f399c15f35508e1
EJ Go <emmanuelle.james.go@gmail.com>
2018-03-28 08:21:41 -0400

    Additional changes

---
+++ Assets/Scripts/GenericClass.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericClass : MonoBehaviour {
+
+    public int hp, ener, atk, dex, def, luk;
+    public float spd;
+    public Animator anim;
+
+    private Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    public void TakeDamage(int amt)
+    {
+        anim.Play("DAMAGED", -1, 0f);
+        if ((hp - (amt - def)) > 0)
+         hp -= (amt - def);
+        else {
+            hp = 0;
+            Destroy(gameObject);
+        }
+    }
+
+    public void Movement()
+    {
+        if (gameObject.CompareTag("Player"))
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(h, 0, v) * spd * Time.deltaTime;
+
+            anim.SetFloat("inputH", h);
+            anim.SetFloat("inputV", v);
+
+            rigid.MovePosition(transform.position + movement);
+        }
+    }
+}
---
+++ Assets/Scripts/IcarusClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcarusClass : GenericClass {
+
+    public IcarusClass()
+    {
+        atk = 10;
+        def = 5;
+        spd = 12.0f;
+    }
+
+    private void Update()
+    {
+        Movement();
+        MeleeAttack();
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            print("I pressed Z!");
+            MeleeAttack();
+        }
+    }
+
+    private void MeleeAttack()
+    {
+        GameObject enemyObj = GameObject.FindGameObjectWithTag("Enemy");
+        GenericClass enemy = enemyObj.GetComponent<GenericClass>();
+        float dist, maxDist = 2.0f;
+
+        dist = Vector3.Distance(transform.position, enemyObj.transform.position);
+        if (dist < maxDist)
+        {
+            print("Enemy in my sight!");
+            anim.Play("MELEE01", -1, 0f);
+            enemy.TakeDamage(atk);
+        }
+    }
+}
